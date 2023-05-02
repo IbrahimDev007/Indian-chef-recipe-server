041ea962bf39bf170643b4ca8738c678516340c6
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-05-03 01:48:49 +0600

    chefe data set

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefs = require('./chief.json');
 
 // enable CORS
 app.use(cors());
 
 // your routes and middleware go here
+app.get('/', (req, res) => {
+    const chefsData = chefs.map(chef => {
+        return {
+            name: chef.name,
+            picture: chef.picture,
+            years_of_experience: chef.years_of_experience,
+            number_of_recipes: chef.number_of_recipes,
+            likes: chef.likes
+        };
+    });
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
+    res.send(chefsData);
 });
+
+
+
+app.listen(port, () => {
+    console.log(`Chiefe API is running on port: ${port}`)
+})
